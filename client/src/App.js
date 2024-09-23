567e53567b82a52a35f6d84e0da373857bbdbf8d
Mohit Tiwari <mohittiwari9933@gmail.com>
2024-09-23 10:28:29 +0530

    All Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./components/Login";
-import UserList from "./components/UserList";
-import Protected from "./components/Protected";
-
+import "./App.css";
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/users" element={<UserList />} />
-          <Route path="/protected" element={<Protected />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <h1>This is Frontend Side</h1>
+    </div>
   );
 }
 
